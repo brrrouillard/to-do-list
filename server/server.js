6254d47fe1d2080eb9f6a5b22154c2fcf80dc3d0
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-03 14:51:37 +0100

    Add home page

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const users = require("./api/users");
+
+app.use("/api/users/", users);
+
+app.listen(port, () => {
+  console.log(`Server runninng on port ${port}`);
+});
