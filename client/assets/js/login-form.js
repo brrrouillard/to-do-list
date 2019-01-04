b1ebc9a44a66adc7fa7b0d183a7cf2b055f1c05b
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-04 10:51:33 +0100

    Add front side register verification

--- client/assets/js/login-form.js
+++ client/assets/js/login-form.js
@@ -5,42 +5,47 @@ const registerPassword = document.querySelector("#register-password-field");
 const api = "http://localhost:8080/api/users/";
 
 const registerRequest = () => {
-    console.log(registerEmail.value);
-    console.log(registerPassword.value);
-  axios
-    .post(api + "/register", {
-      email: registerEmail.value,
-      password: registerPassword.value
-    })
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  if (registerEmail.value == "" || registerPassword.value == "") {
+    document.querySelector("#register-error-message").innerHTML =
+      "Please enter a valid email and password";
+  } else {
+    axios
+      .post(api + "/register", {
+        email: registerEmail.value,
+        password: registerPassword.value
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 };
 
 submitRegister.addEventListener("click", registerRequest);
 
-
-
 /*
-* NAVBAR
-*/
-const login = document.querySelectorAll('.login-navbar')
-const register = document.querySelectorAll('.register-navbar')
+ * NAVBAR
+ */
+const login = document.querySelectorAll(".login-navbar");
+const register = document.querySelectorAll(".register-navbar");
 
 const showRegister = () => {
-    document.querySelector('.login-page').setAttribute('style', 'display: none');
-    document.querySelector('.register-page').setAttribute('style', 'display: block');
-    console.log("hello");
-}
+  document.querySelector(".login-page").setAttribute("style", "display: none");
+  document
+    .querySelector(".register-page")
+    .setAttribute("style", "display: block");
+  console.log("hello");
+};
 
 const showLogin = () => {
-    document.querySelector('.register-page').setAttribute('style', 'display: none');
-    document.querySelector('.login-page').setAttribute('style', 'display: block');
-    console.log("noo");
-}
+  document
+    .querySelector(".register-page")
+    .setAttribute("style", "display: none");
+  document.querySelector(".login-page").setAttribute("style", "display: block");
+  console.log("noo");
+};
 
-register.forEach(e => e.addEventListener('click', showRegister));
-login.forEach(e => e.addEventListener('click', showLogin));
+register.forEach(e => e.addEventListener("click", showRegister));
+login.forEach(e => e.addEventListener("click", showLogin));
