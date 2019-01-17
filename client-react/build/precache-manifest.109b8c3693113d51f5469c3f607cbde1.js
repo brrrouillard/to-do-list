a77e285a01414687f5b70d1f1768cf32a288c1ff
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-17 17:08:23 +0100

    Add build

---
+++ client-react/build/precache-manifest.109b8c3693113d51f5469c3f607cbde1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c20ada7f2f4f9f01e9b",
+    "url": "/static/js/main.2c20ada7.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "2c20ada7f2f4f9f01e9b",
+    "url": "/static/css/main.22679074.chunk.css"
+  },
+  {
+    "revision": "76a89c510a735be73dab2bf499a2dceb",
+    "url": "/index.html"
+  }
+];
